fafd03a14ab4a235133df59448ee205eba215544
caiyc <1376648068@qq.com>
2015-02-10 12:39:08 +0800

    dsdscvc

---
+++ SingleTon/SingleTon/Singleton.h
@@ -0,0 +1,13 @@
+//
+//  Singleton.h
+//  SingleTon
+//
+//  Created by caiyc on 15/1/5.
+//  Copyright (c) 2015年 mingthink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Singleton : NSObject
++(Singleton *)shareinstance;
+@end
---
+++ SingleTon/SingleTon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleTon
+//
+//  Created by caiyc on 15/1/5.
+//  Copyright (c) 2015年 mingthink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
